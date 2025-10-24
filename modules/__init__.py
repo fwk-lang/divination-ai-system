d2f16ee84287fa4fb6762812032e3d60eb474780
fwk-lang <fwk@g.lwfss.edu.hk>
2025-10-24 13:50:48 +0800

    Fix module imports - use correct calculator filenames

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,4 @@
 # Divination Modules
-from . import bazi
-from . import ziwei
+from . import bazi_calculator as bazi
+from . import ziwei_calculator as ziwei
+from . import yijing_calculator as yijing
