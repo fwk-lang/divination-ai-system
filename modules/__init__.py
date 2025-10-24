06c515dd223e1915e38b338ebfdfbe1c50a2c449
fwk-lang <fwk@g.lwfss.edu.hk>
2025-10-24 17:02:54 +0800

    Temporarily disable ziwei_calculator import to debug deployment hang

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,7 @@
 # Divination Modules
 from . import bazi_calculator
-from . import ziwei_calculator
+# Temporarily comment out ziwei to test if it's causing the hang
+# from . import ziwei_calculator
 from . import yijing_calculator
 
 # Create wrapper objects to match app.py's expected API
@@ -11,7 +12,12 @@ class bazi:
         return bazi_calculator.bazi.calculate(**kwargs)
 
 class ziwei:
-    """Wrapper for ziwei calculator"""
+    """Temporary stub for ziwei - will be implemented after fixing import issue"""
     @staticmethod
     def calculate_ziwei(**kwargs):
-        return ziwei_calculator.calculate_ziwei(**kwargs)
+        return {
+            'status': 'temporary_stub',
+            'message': '紫微斗數模塊暫時禁用以調試部署問題',
+            'palaces': {},
+            'main_star_analysis': '功能開發中...'
+        }
